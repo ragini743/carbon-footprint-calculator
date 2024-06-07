779c94a88990e3f89583f0ba85bc10baee16485e
Ragini <raginiv743@gmail.com>
2024-06-07 23:21:26 +0530

    create a carbonFootPrintResult component:

---
+++ src/component/CarbonFootprintResult.js
@@ -0,0 +1,17 @@
+// src/CarbonFootprintResult.js
+import React from 'react';
+
+const CarbonFootprintResult = ({ result }) => {
+  return (
+    <div>
+      <h2>Your Carbon Footprint</h2>
+      <p>Electricity: {result.electricity} kg CO2</p>
+      <p>Gas: {result.gas} kg CO2</p>
+      <p>Water: {result.water} kg CO2</p>
+      <p>Travel: {result.travel} kg CO2</p>
+      <h3>Total: {result.total} kg CO2</h3>
+    </div>
+  );
+};
+
+export default CarbonFootprintResult;
